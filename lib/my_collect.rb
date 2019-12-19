100d19b3d99f13ae8b659c84bcf27d89913ccfce
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 23:59:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,11 @@
 def my_collection(collection)
-new_collection = [ ]
-i = 0
-    while i < collection.length
-     new_collection  << (yield(collection[i]))
+ new_collection = [ ]
+ i = 0
+ while i < collection.length
+     new_collection << yield(collection[i])
       i = i + 1
     end
     return new_collection
-
 end
 
 
