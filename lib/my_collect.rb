ba33f2ddd0bd81d95041609a90d8ce1b8c4dbaf4
Damely Tineo <damely.tineo07@gmail.com>
2019-12-20 00:12:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
-
+new_collection = []
  i = 0
  while i < collection.length
-      yield(collection[i])
+      new_collection << yield(collection[i])
       i = i + 1
     end
-    return collection
+    return new_collection
 end
